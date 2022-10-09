a289b0793adf782c2536536ea6c59dacf18720f8
Crypto <mayanksethi242008@gmail.com>
2022-10-09 22:19:58 +0530

    + task cards style & addition btn; + side navbar

--- flaskr/static/js/script.js
+++ flaskr/static/js/script.js
@@ -1,8 +1,21 @@
-let btn = document.getElementById("create-new")
-let failCheck = false
+// check if any task addition button is pressed
+const todo_add = document.getElementById('add-todo')
+const prog_add = document.getElementById('add-prog')
+const upcoming_add = document.getElementById('add-upcoming')
+const urgent_add = document.getElementById('add-urgent')
 
-btn.addEventListener("click", event => {
-        alert("event")   
-}
-)
+todo_add.addEventListener('click', event => {
+        alert('todo clicked')
+})
 
+prog_add.addEventListener('click', event => {
+        alert('prog clicked')
+})
+
+upcoming_add.addEventListener('click', event=>{
+        alert('upcoming clicked')
+})
+
+urgent_add.addEventListener('click', event => {
+        alert('urgent clicked')
+})
